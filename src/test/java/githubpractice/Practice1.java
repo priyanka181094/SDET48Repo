0f8d8306bfa2fb553312156b798a834659d72502
Priyanka Ghosh <Priyanka Ghosh@Priyanka>
2023-05-31 16:41:09 +0530

    commit1 githubpractice

---
+++ src/test/java/githubpractice/Practice1.java
@@ -0,0 +1,25 @@
+package githubpractice;
+
+import org.testng.annotations.Test;
+
+public class Practice1 {
+	@Test
+	public void insert()
+	{
+		System.out.println("insert  product");
+	}
+	@Test
+	public void delete()
+	{
+		System.out.println("delete product");
+	}
+	@Test
+	public void create()
+	{
+		System.out.println("create product");
+	}
+
+}
+
+
+
---
+++ src/test/java/githubpractice/Practice2.java
@@ -0,0 +1,19 @@
+package githubpractice;
+
+import org.testng.annotations.Test;
+
+public class Practice2 {
+	@Test
+	public void name()
+	{
+		System.out.println("my name is priyanka");
+		System.out.println("I live in banaglore");
+		
+		
+		System.out.println("i am studying");
+		
+		
+		
+	}
+
+}
